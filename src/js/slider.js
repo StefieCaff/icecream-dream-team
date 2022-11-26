9ba35456b3b33f6940664c08a49968506bb0ddfa
andangelina <110055335+andangelina@users.noreply.github.com>
2022-11-25 19:38:51 -0500

    Update slider.js

---
+++ src/js/slider.js
@@ -0,0 +1,18 @@
+$('.reviews__box').slick({
+    infinite: true,
+    slidesToShow: 1,
+slidesToScroll: 1,
+dots: true,
+arrows: false,
+autoplay: true,
+autoplaySpeed: 4000,
+pauseOnFocus: true,
+pauseOnHover: true,
+pauseOnDotsHover:true,
+});
+  
+// $('.reviews__box').slick({
+//     infinite: true,
+//     dots: true,
+//     arrows: false,
+// });
