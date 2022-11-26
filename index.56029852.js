f0d66f2260d60ef916adc13f8b70a0d73e41300b
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2022-11-26 00:40:26 +0000

    Deploying to gh-pages from @ StefieCaff/icecream-dream-team@8957661b8e73aaeaca8536554ee45461df3e0aa6 🚀

---
+++ index.56029852.js
@@ -0,0 +1,2 @@
+$(".reviews__box").slick({infinite:!0,slidesToShow:1,slidesToScroll:1,dots:!0,arrows:!1,autoplay:!0,autoplaySpeed:4e3,pauseOnFocus:!0,pauseOnHover:!0,pauseOnDotsHover:!0});
+//# sourceMappingURL=index.56029852.js.map
